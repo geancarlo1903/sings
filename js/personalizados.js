32a563213668a2b8a1803a393ff81a9e542eb309
geancast <geancarlo1903cy@gmail.com>
2021-12-06 23:54:45 -0500

    actualizacion 6/12/21 11:54 pm

--- js/personalizados.js
+++ js/personalizados.js
@@ -1,4 +1,4 @@
-function ventana(N){
+function ventanaP(N){
     switch(N){
         
         case 1:/*pagina stikers*/
@@ -28,6 +28,40 @@ function ventana(N){
         
         case 6:/*pagina magnets*/
         var url = "more.html";
+        window.open(url,"target");
+        break;
+    }
+}
+function ventanaPU(N){
+    switch(N){
+        
+        case 1:/*pagina banner*/
+        var url = "banner.html";
+        window.open(url,"target");
+        break;
+
+        case 2:/*pagina labels*/
+        var url = "rigid.html";
+        window.open(url,"target");
+        break;
+        
+        case 3:/*pagina magnets*/
+        var url = "adhesive.html";
+        window.open(url,"target");
+        break;
+        
+        case 4:/*pagina buttons*/
+        var url = "handheld.html";
+        window.open(url,"target");
+        break;
+        
+        case 5:/*pagina magnets*/
+        var url = "magnet.html";
+        window.open(url,"target");
+        break;
+        
+        case 6:/*pagina magnets*/
+        var url = "music.html";
         window.open(url,"target");
         break;
     }
